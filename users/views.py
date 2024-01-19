646dc3f276127f19aed637e66fe13f80da728188
Артур <informal201144@gmai;.com>
2024-01-19 11:14:03 +0500

    added users CRUD

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from users.models import User
+from users.serializers import UserSerializers
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializers
+    queryset = User.objects.all()
